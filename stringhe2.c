4657e959444b46b32ebb7623806e52ba88158c9c
Niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-02-08 10:23:20 +0100

    migliorato es stringhe

--- stringhe2.c
+++ stringhe2.c
@@ -3,9 +3,14 @@
 int main(int argc, char *argv[])
 {
     char nome[30]; // hanno /0 alla fine
-    int i;
+    int i = 0;
     printf("Inserisci il tuo nome \n");
-    scanf("%s", nome); //no & perché puntatori
+    scanf("%s", nome); // no & perché puntatori
     printf("Il tuo nome è %s", nome);
+    while (nome[i] != '\0') // per vedere quanto è lunga stringa
+    {
+        i++;
+    }
+    printf("La stringa è lunga %d \n", i);
     return 0;
 }
