483cd765aaeb82e2609b52205419ff50f0592a77
Niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-02-15 09:47:19 +0100

    fatto inizio delle stringhe

--- esClasee/stringhe3.c
+++ esClasee/stringhe3.c
@@ -2,6 +2,9 @@
 #include <string.h>
 int main(int argc, char *argv[])
 {
-    int i = 0, ;
+    int i = 0, cons = 0, voc = 0, lung = 0;
+    char stringa[12];
+    printf("Inserire stringa");
+    scanf("%s", stringa);
     return 0;
 }
