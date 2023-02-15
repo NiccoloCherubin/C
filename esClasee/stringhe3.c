9179468d374074323bcab9449c8c31cb1aec0093
Niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-02-15 09:57:42 +0100

    finito es stringhe3

--- esClasee/stringhe3.c
+++ esClasee/stringhe3.c
@@ -2,9 +2,27 @@
 #include <string.h>
 int main(int argc, char *argv[])
 {
-    int i = 0, cons = 0, voc = 0, lung = 0;
-    char stringa[12];
-    printf("Inserire stringa");
-    scanf("%s", stringa);
+    int i = 0, cons = 0, voc = 0, lung = i;
+    char stringa[20]; // dichiarazione stringa
+    printf("Inserire stringa \n");
+    scanf("%s", stringa); // input della stringa
+    while (str[i] != '\0')
+    {
+        i++; // calcolo lunghezza stringa
+    }
+    lung = i;
+    for (int i = 0; i < lung; i++)
+    {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+        {
+            voc++;
+        }
+        else
+        {
+            cons++;
+        }
+        printf("Le vocali sono %d \n", voc);
+        printf("Le consonanti sono %d \n", cons);
+    }
     return 0;
 }
