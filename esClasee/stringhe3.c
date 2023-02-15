a9577142f1b809c2f63eb3b113b56c9670b154b3
Niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-02-15 10:06:26 +0100

    fatto inizio delle stringhe

--- esClasee/stringhe3.c
+++ esClasee/stringhe3.c
@@ -2,10 +2,10 @@
 #include <string.h>
 int main(int argc, char *argv[])
 {
-    int i = 0, cons = 0, voc = 0, lung = i;
-    char stringa[20]; // dichiarazione stringa
+    int i = 0, cons = 0, voc = 0, lung = i, num = 0;
+    char str[20]; // dichiarazione stringa
     printf("Inserire stringa \n");
-    scanf("%s", stringa); // input della stringa
+    scanf("%s", str); // input della stringa
     while (str[i] != '\0')
     {
         i++; // calcolo lunghezza stringa
@@ -19,10 +19,18 @@ int main(int argc, char *argv[])
         }
         else
         {
-            cons++;
+            if (str[i] != 'a' || str[i] != 'e' || str[i] != 'i' || str[i] != 'o' || str[i] != 'u' && (str[i] > 'a' || str[i] < 'z')) // per codice ascii (compreso tra lettere)
+            {
+                cons++;
+            }
+            else
+            {
+                num++;
+            }
         }
+    }
         printf("Le vocali sono %d \n", voc);
         printf("Le consonanti sono %d \n", cons);
+        printf("I numeri sono %d \n", num);
+        return 0;
     }
-    return 0;
-}
