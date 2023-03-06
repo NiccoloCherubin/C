9706fb59329f92c14528c8c0cc448dc91292359f
Niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-03-06 09:09:44 -0800

    finito tutto

--- esCasa/esDoppie.c
+++ esCasa/esDoppie.c
@@ -2,17 +2,17 @@
 #include <string.h>
 int main(int argc, char *argv[])
 {
-    char stringa[20], carattere;
+    char stringa[20];
     int doppie = 0; // per contare quante volte è presente il carattere nella stringa
     // inserimento stringa
     printf("inserire stringa\n");
-    scanf("%s", stringa);    
-    
-    for (int i = 0; i < strlen(stringa) -1; i++)
+    scanf("%s", stringa);
+
+    for (int i = 0; i < strlen(stringa) - 1; i++)
     {
         if (stringa[i] == stringa[i + 1]) // verifico se ci sono doppie
         {
-            doppie+= 2; // conto numero di doppie
+            doppie += 2; // conto numero di doppie
         }
     }
     // stampa del numero di doppie
