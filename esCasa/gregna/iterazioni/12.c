9776cd720753b9a108c34cf50fde1350b109b5f5
Niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-03-21 08:43:54 -0700

    es 11 finito

---
+++ esCasa/gregna/iterazioni/12.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    int N;
+    int somma = 0;
+    do
+    {
+        printf("Inserire un numero maggiore di 0 \n");
+        scanf("%d", N);
+    } while (N <= 0);
+    do
+    {
+        somma += N;
+        N--;
+    } while (N > 0);
+    printf("La somma è %d \n", somma);
+    return 0;
+}
