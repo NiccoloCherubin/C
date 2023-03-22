8a09d02d3a47238cd4c6ccbf3e0d225ec7d672df
Niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-03-22 11:00:22 +0100

    finito esercizio eliminziione carattre da linea di comando

--- esClasee/VettStr/2.c
+++ esClasee/VettStr/2.c
@@ -8,16 +8,17 @@ int main(int argc, char *argv[])
         printf("Errore argomenti \n");
     }
     else
-    {        
-        for (int i = 0; i < argc - 1; i++) // - 1 per guardare solo la stringa
+    {
+        for (int i = 0; i < strlen(argv[1]) - 1; i++)
         {
-            if (argv[2][i] == argv[3])
+            if (argv[1][i] == argv[2][0])
             {
-                argv[2][i] = "";
+                argv[1][i] = argv[1][i + 1];
             }
         }
-        // stampo stringa finale
-        printf("\n %s", stringa);
+        argv[1][strlen(argv[1]-1)] = ' ';
+
+        printf("\n %s", argv[1]);
     }
     return 0;
 }
