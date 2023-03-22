94d0a238fc68f35032e5f90fc0143d52e9444baa
Niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-03-22 09:53:00 +0100

    rework

--- esCasa/padovani/esDoppieShell.c
+++ esCasa/padovani/esDoppieShell.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 int main(int argc, char *argv[])
-{    
+{
     int doppie = 0; // per contare quante volte è presente il carattere nella stringa
-    if (argc != 2) // verifico quantità argomenti inseriti
+    if (argc != 2)  // verifico quantità argomenti inseriti
     {
         printf("Errore argomenti");
     }
     else
     {
-        for (int i = 0; i < argc ; i++)
+        for (int i = 0; i < argc; i++)
         {
             if (argv[2][i] == argv[2][i + 1]) // verifico se ci sono doppie
             {
