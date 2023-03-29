49224e190734d3d330afd954538652de3bb68790
Niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-03-29 09:47:18 +0200

    tolto \n

--- esClasee/VettStr/2.c
+++ esClasee/VettStr/2.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
                 cont++;
             }
         }        
-            printf("\n %s", supporto);            
+            printf("%s", supporto);            
     }
     return 0;
 }
