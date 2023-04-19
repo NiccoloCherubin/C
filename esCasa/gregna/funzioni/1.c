55f13cd7196c85e5e1243e51ae29969d2401b2bd
Niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-04-19 11:53:47 +0200

    rifatto 1

--- esCasa/gregna/funzioni/1.c
+++ esCasa/gregna/funzioni/1.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 int Somma(int a, int b)
 {
-    return a+b;
+    return a + b;
+}
+int Verifica(int valore)
+{
+    if (valore <= 0)
+    {
+        return -1;
+    }
+    else
+    {
+        return valore;
+    }
 }
 int main(int argc, char *argv[])
 {
-    int a,b;
-    printf("Inserisci numero 1 \n");
-    scanf("%d", &a);
-    printf("Inserisci numero 2 \n");
-    scanf("%d", &b);
-    printf("La somma è %d \n",Somma(a,b));
+    int a, b;
+    do
+    {
+        printf("Inserisci numero 1 \n");
+        scanf("%d", &a);
+    } while (Verifica(a) == -1);
+    do
+    {
+        printf("Inserisci numero 2 \n");
+        scanf("%d", &b);
+    } while (Verifica(b) == -1);
+    printf("La somma è %d \n", Somma(a, b));
     return 0;
 }
