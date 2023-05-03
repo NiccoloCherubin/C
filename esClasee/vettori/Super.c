ba16291cb59dfc52bb971b0d65932f13e95e75af
Niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-05-03 10:17:00 +0200

    Riempimento cont  controllo

--- esClasee/vettori/Super.c
+++ esClasee/vettori/Super.c
@@ -4,14 +4,24 @@
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+int Controllo(int a, int cont)
+{
+    do
+    {
+        printf("Inserisci elemento %d \n", cont);
+        scanf("%d", &a);
+        if (a == -1)
+        {
+            break;
+        }
+    } while (a < 0);
+}
 int Riempi(int array[], int n)
 {
-    int temp;
+    int temp; // variabile temporanea per non sporcare array
     for (int i = 0; i < SHRT_MAX; i++)
     {
-        printf("Inserisci elemento %d \n", i);
-        scanf("%d", &temp);
+        temp = Controllo(temp, i);
         if (temp == -1)
         {
             break;
