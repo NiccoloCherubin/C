54433d0138cd9d2f39318775e7237abc399483a7
Niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-05-17 09:40:40 +0200

    es 28 finito

---
+++ esCasa/gregna/funzioni/28.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void Funzione(char str1[], char str2[])
+{
+    char *p = str2;
+    for (int i = 0; i < strlen(str1); i++)
+    {
+        if (isdigit(str1[i]))
+        {
+            *p = str1[i];
+            p++;
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    char str1[100], str2[100] = {0};
+    printf("inserire la stringa: ");
+    scanf("%s", str1);
+
+    Funzione(str1, str2);
+
+    if (str2[0] != 0)
+        printf("\nstr2: %s\n", str2);
+    else
+        printf("\nNon ci sono numeri nella stringa\n");
+
+    return 0;
+}
