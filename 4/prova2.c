ec683e6997e62a4373ffff03cf332a8e128933fc
niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-09-14 08:42:21 -0700

    esercizio 2

--- 4/prova2.c
+++ 4/prova2.c
@@ -1,24 +1,43 @@
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
-    // compresi tra 30 e 50 e calcolare somma
-    int limite1, limite2, n, somma, i = 0;
+
+    int limite1, limite2, n, somma = 0, i = 0;
     const short min = 30, max = 50;
-    while (i < 5)
+    // inserimento limiti
+    do
     {
-        printf("Inserire numero 1 \n");
+        printf("Inserire il primo limite compreso tra 30 e 50\n");
         scanf("%d", &limite1);
-        printf("Inserire numero 2 \n");
+    } while (limite1 < min || limite1 > max);
+    do
+    {
+        printf("Inserire il secondo limite compreso tra 30 e 50\n");
         scanf("%d", &limite2);
-        if (limite1 <= max && limite2 <= max && limite1 >= min && limite2 >= min)
+    } while (limite2 < min || limite2 > max);
+    // confronto e swap limiti e rendo limite1 sempre il minore
+    if (limite1 >= limite2)
+    {
+        i = limite1;
+        limite1 = limite2;
+        limite2 = i;
+    }
+    // inserimento numeri
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Inserire %d numero \n", i + 1);
+        scanf("%d", &n);
+        if (n >= limite1 && n <= limite2)
         {
-            i++;
+            somma += n;
         }
         else
         {
-            printf("\n I numeri devono esserre compresi tra %d e %d", &limite1, &limite2);
-        }
+            printf("Il numeri non è compreso tra i limiti \n");
+        }        
     }
+    //stampa della somma dei numeri
+    printf("La somma dei numeri compresi tra i limiti è uguale a %d", somma);
 
     return 0;
 }
