10c3eb53bde05a892183af8eaeccb49c4f4fdb6f
Niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-09-21 08:57:55 +0200

    argv

--- 3/esCasa/gregna/funzioni/3.c
+++ 3/esCasa/gregna/funzioni/3.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-int indice(int argc, char *argv[], int n, int start)
-=======
-int Ricerca(int argc, char *argv[], int n, int start)
->>>>>>> 55f13cd7196c85e5e1243e51ae29969d2401b2bd
-=======
 int Ricerca(int argc, char *argv[], int n)
->>>>>>> 1486783a6b5c477a66698a955cdd7c96c4cf090f
 {
     for (int i = 1; i < argc; i++)
     {
@@ -23,29 +14,17 @@ int Ricerca(int argc, char *argv[], int n)
 
 int main(int argc, char *argv[])
 {
-<<<<<<< HEAD
     int n;
     printf("Inserisci il numero da cercare nell'array: ");
     scanf("%d", &n);
     n = indice(argc, argv, n, 1);
     if (n == -1)
-=======
-    int numero;
-    printf("Inserisci il numero da cercare nell'array: ");
-    scanf("%d", &numero);
-    numero = Ricerca(argc, argv, numero);
-    if (numero == -1)
->>>>>>> 55f13cd7196c85e5e1243e51ae29969d2401b2bd
     {
         printf("Numero non trovato\n");
     }
     else
     {
-<<<<<<< HEAD
         printf("Numero trovato in posizione %d\n", n);
-=======
-        printf("Numero alla posizione %d\n", numero);
->>>>>>> 55f13cd7196c85e5e1243e51ae29969d2401b2bd
     }
     return 0;
 }
--- 4/prova3.c
+++ 4/prova3.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-void Main()
+#include <stdlib.h>
+int main(int argc, char *argv[])
 {
-    printf("Hello world");
+    printf("argc: %d \n", argc);
+    printf("Parametri argv \n");
+    for (int i = 1; i < argc; i++)
+    {
+        printf("%d. %s \n", i,argv[i]);
+    }
 }
