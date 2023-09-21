e32eb933963b3c505d809905c1ca2473f4c19d1a
Niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-09-21 08:03:57 +0200

    italianizzato

--- 4/prova2.c
+++ 4/prova2.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
         }
         else
         {
-            printf("Il numeri non è compreso tra i limiti \n");
+            printf("Il numero non è compreso tra i limiti \n");
         }        
     }
     //stampa della somma dei numeri
