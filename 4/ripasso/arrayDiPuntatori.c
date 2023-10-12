93700712549c3a16ee774e46e77d2f2b85c39bb6
Niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-10-12 09:13:16 +0200

    array puntatori a strutture

--- 4/ripasso/arrayDiPuntatori.c
+++ 4/ripasso/arrayDiPuntatori.c
@@ -7,25 +7,25 @@ typedef struct // definisco struct
     char cognome[20];
     int media;
 } Studente;
-void stampa(Studente *studenti, const int len)
+void stampa(Studente *studenti[], const int len)
 {
     // stampa array
     for (int i = 0; i < len; i++)
     {
-        printf("Nome : %s \n", (studenti->nome + i));
-        printf("Cognome : %s \n", (studenti->cognome + i));
-        printf("Media : %d \n", (studenti->media + i));
+        printf("Nome : %s \n", (studenti[i]->nome));
+        printf("Cognome : %s \n", (studenti[i]->cognome));
+        printf("Media : %d \n", (studenti[i]->media));
     }
 }
-void popola(Studente *studenti, int i)
+void popola(Studente *studenti[], int i)
 {
     // popola array
     printf("Inserire nome dello studente %d \n", i + 1);
-    scanf("%s", studenti[i].nome); // valore puntato da puntatori
+    scanf("%s", studenti[i]->nome); // valore puntato da puntatori
     printf("Inserire cognome dello studente %d\n", i + 1);
-    scanf("%s", studenti[i].cognome); // valore puntato da puntatori
+    scanf("%s", studenti[i]->cognome); // valore puntato da puntatori
     printf("Inserire media dello studente %d \n", i + 1);
-    scanf("%d", &studenti[i].media); // valore puntato da puntatori
+    scanf("%d", &studenti[i]->media); // valore puntato da puntatori
 }
 int main(int argc, char *argv[])
 {
@@ -35,9 +35,9 @@ int main(int argc, char *argv[])
     for (int i = 0; i < lunghezza; i++)
     {
         studenti[i] = (Studente *)malloc(sizeof(Studente)); // alloco memoria
-        popola(studenti[0], i);
+        popola(studenti, i);
     }
-    stampa(studenti[0], lunghezza);
+    stampa(studenti, lunghezza);
     //  libero memoria occupata
     for (int i = 0; i < lunghezza; i++)
     {
