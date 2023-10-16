7d8a1b5498aa514ea3518d88a39f0b99111bc291
niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-10-16 07:30:18 -0700

    biblioteca

---
+++ 4/ripasso/biblioteca.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct
+{
+    char autore[20];
+    double prezzo;
+    char titolo[20];
+} biblioteca;
+void popola(biblioteca *libro, int i)
+{
+    printf("Inserire autore del libro %d \n", i + 1);
+    scanf("%s", libro->autore); // valore puntato da puntatori
+    printf("Inserire titolo del libro %d\n", i + 1);
+    scanf("%s", libro->titolo); // valore puntato da puntatori
+    printf("Inserire prezzo del libro %d \n", i + 1);
+    scanf("%lf", &libro->prezzo); // valore puntato da puntatori
+}
+void stampa(biblioteca *libri, int lunghezza)
+{
+    for (int i = 0; i < lunghezza; i++)
+    {
+        printf("Libro %d:\n", i + 1);
+        printf("Autore: %s\n", libri[i].autore);
+        printf("Titolo: %s\n", libri[i].titolo);
+        printf("Prezzo: %lf\n", libri[i].prezzo);
+    }
+}
+void sconto(biblioteca *libri, int lunghezza, int scontoPercentuale)
+{
+    for (int i = 0; i < lunghezza; i++)
+    {
+        libri[i].prezzo = libri[i].prezzo - (libri[i].prezzo * scontoPercentuale / 100);
+    }
+}
+int main(int argc, char *argv[])
+{
+
+    const int lunghezza = 1;
+    biblioteca libri[lunghezza];
+    int scontoPercentuale;
+    for (int i = 0; i < lunghezza; i++)
+    {
+        popola(&libri[i], i);
+    }
+    printf("PRIMA DELLO SCONTO");
+    stampa(libri, lunghezza);
+    printf("Inserisci lo sconto percentuale: ");
+    scanf("%d", &scontoPercentuale);
+
+    sconto(libri, lunghezza, scontoPercentuale);
+
+    printf("Libri DOPO lo sconto:\n");
+    stampa(libri, lunghezza);
+
+    return 0;
+}
