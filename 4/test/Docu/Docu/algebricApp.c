d3af2c46aeaf793f4b205dd3f6285c56f4d407fd
Niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2023-11-02 08:39:29 +0100

    html+

--- 4/test/Docu/Docu/algebricApp.c
+++ 4/test/Docu/Docu/algebricApp.c
@@ -1,25 +1,39 @@
 #include <stdio.h>
 
+/// @brief compute the sum of the two numbers
+/// @param a first parameter
+/// @param b second parameter
+/// @return the sum of the two numbers
 int add(int a, int b)
 {
     return a+b;
 }
 
+/// @brief compute the multiplication of the two numbers
+/// @param a first parameter
+/// @param b second parameter
+/// @return the multiplication of the two numbers
 int mul(int a, int b)
 {
     return a*b;
 }
-
+/// @brief compute the minus of the two numbers
+/// @param a first parameter
+/// @param b second parameter
+/// @return the minus of the two numbers
 int sub (int a, int b)
 {
     return a-b;
 }
-
+/// @brief compute the division of the two numbers
+/// @param a first parameter
+/// @param b second parameter
+/// @return the division of the two numbers
 double division(int a, int b)
 {
     return a/b;
 }
-
+/// @brief main function
 void main(){
     printf("%d\n",add(6,3));
     printf("%d\n",mul(6,3));
