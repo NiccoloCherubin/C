5a51ebaea2f628adb0661cc141486fb433dbd811
Niccolo Cherubin <niccolo.cherubin@iisviolamarchesini.edu.it>
2024-10-11 09:19:08 +0200

    prime socket

---
+++ 5/ripasso/es4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define N_BOOKS 40
+
+typedef struct {
+  char title[50];
+  char author[50];
+  int year;
+  float price;
+  char type[40];
+} Book;
+
+int main(int argc, char **argv) {
+
+    Book library[N_BOOKS];
+    return 0;
+ }
