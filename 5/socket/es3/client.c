198223020e011640c37c742b8628cdde77a9d8bb
Niccolo Cherubin <niccolo.cherubin@iisviolamarchesini.edu.it>
2024-10-18 09:11:28 +0200

    palindromia

---
+++ 5/socket/es3/client.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define DIM 1024
+#define SERVERPORT 1313
+
+int main(int argc, char **argv)
+{
+    struct sockaddr_in servizio;
+    int socketfd;
+    char str[DIM]; //dove verrà memorizzata la stringa da inviare al server
+
+    int palindroma = 0; // variabile che dirà se è palindroma oppure no
+
+    servizio.sin_family = AF_INET;
+    servizio.sin_addr.s_addr = htonl(INADDR_ANY); //sfrutta API del SO
+    servizio.sin_port = htons(SERVERPORT);
+
+    socketfd = socket(AF_INET,SOCK_STREAM, 0);
+
+    //verifico se connessione è andata a buon fine
+    if(connect(socketfd, (struct sockaddr*)&servizio, sizeof(servizio)) < 0)
+    {
+        exit(-1);
+    }
+
+    //faccio inserire stringa all'utente
+    printf("Inserisci la stringa \n");
+    scanf("%s", str);
+
+    //scrivo al server stringa inserita dall'utente
+    write(socketfd, str, sizeof(str));
+
+    //leggo dal server se la stringa è palindroma
+    read(socketfd,&palindroma , sizeof(int));
+
+    printf("palindromo = %d\n",palindroma);
+
+    if(palindroma)
+    {
+        printf("La stringa inserita è palindroma\n");
+    }
+    else
+    {
+        printf("La stringa inserita non è palindroma\n");
+    }
+
+    //chiudo connessione 
+    close(socketfd);
+
+    return 0;
+}
---
+++ 5/socket/es3/server.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+
+#define DIM 1024
+#define SERVERPORT 1313
+
+int isNotPalindroma(char *str)
+{
+    for(int i = 0; i < strlen(str)/2;i++)
+    {
+        if(str[i] != str[strlen(str)-i-1])
+        {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int main(int argc, char ** argv)
+{    
+    struct sockaddr_in servizio, addClient;
+    int socketfd, soa, clientLen = sizeof(servizio);
+    char str[DIM];
+    int palindromo = 0;
+
+    //definizione dati del socket
+    servizio.sin_family = AF_INET;
+    servizio.sin_port = htons(SERVERPORT);
+    servizio.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    socketfd = socket(AF_INET,SOCK_STREAM,0);
+
+    if(bind(socketfd, (struct sockaddr*)&servizio, sizeof(servizio)) < 0)
+    {
+        exit(-1);
+    }
+
+    //metto server in ascolto fino a 10 client
+    listen(socketfd, 10); 
+
+    while (1)
+    {
+        printf("Server in asoclto \n");
+
+        //pulisco lo stream
+        fflush(stdout);
+
+        //accetto connessione dal client
+        soa = accept(socketfd, (struct sockaddr*)&addClient, &clientLen);
+
+        //leggo da client
+        read(soa, str, sizeof(str));
+
+        palindromo = isNotPalindroma(str);
+
+        printf("palindromo = %d \n", palindromo);
+
+        //scrivo al client
+        write(soa, &palindromo, sizeof(int));
+
+        close(soa);
+    }
+
+
+    return 0;
+}
