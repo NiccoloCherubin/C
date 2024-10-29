7e1d53282a8aa62191b1b20104f8965b232929c1
niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2024-10-29 13:55:27 -0700

    es 7

--- 5/socket/es7/server.c
+++ 5/socket/es7/server.c
@@ -8,13 +8,17 @@
 #define SERVERPORT 1314
 #define DIM 50
 
-// FUNZIONE CHE CALCOLA LA SOMMA
+// FUNZIONE CHE CALCOLA LA SOMMA SOLO DEI NUMERI DISPARI
 
-int sum(int *vett) {
+int sumOdd(int *vett, int *countOdd) {
   int somma = 0;
+  *countOdd = 0;
 
   for (int i = 0; i < DIM; i++) {
-    somma += vett[i];
+    if (vett[i] % 2 != 0) { // verifica se il numero è dispari
+      somma += vett[i];
+      (*countOdd)++; // incrementa il contatore dei numeri dispari
+    }
   }
 
   return somma;
@@ -24,7 +28,7 @@ int main(int argc, char **argv) {
   struct sockaddr_in servizio, addClient;
   int socketfd, soa, clientLen = sizeof(servizio);
   int arr[DIM];
-  int somma;
+  int somma, countOdd;
   float media;
 
   servizio.sin_family = AF_INET;
@@ -49,32 +53,28 @@ int main(int argc, char **argv) {
 
     printf("Server in ascolto \n");
 
-    // pulisco lo stream
-
     // accetto connessione dal client
     soa = accept(socketfd, (struct sockaddr *)&addClient, &clientLen);
 
     // leggo array dal client
-    if(read(soa, arr, sizeof(arr))<0)
-    {
+    if(read(soa, arr, sizeof(arr)) < 0) {
         perror("Errore lettura array dal client \n");
         exit(-1);
     }
 
-    somma = sum(arr);
-    media = (float)somma / DIM;
+    // calcolo somma e media dei numeri dispari
+    somma = sumOdd(arr, &countOdd);
+    media = countOdd > 0 ? (float)somma / countOdd : 0; // evita divisione per zero
 
-    printf("media : %f\n", media);
-    printf("somma : %d\n", somma);
+    printf("Media dei numeri dispari: %f\n", media);
+    printf("Somma dei numeri dispari: %d\n", somma);
 
     // mando al client i risultati
-    if(write(soa, &somma, sizeof(somma))<0)
-    {
+    if(write(soa, &somma, sizeof(somma)) < 0) {
         perror("Errore scrittura somma al client \n");
         exit(-1);
     }
-    if(write(soa, &media, sizeof(media)) < 0)
-    {
+    if(write(soa, &media, sizeof(media)) < 0) {
         perror("Errore scrittura media al client \n");
         exit(-1);
     }
