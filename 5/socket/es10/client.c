8ea22d8c748ab331f3808759959e6e568f167759
Niccolo Cherubin <niccolo.cherubin@iisviolamarchesini.edu.it>
2024-10-30 10:01:46 +0100

    es 10

---
+++ 5/socket/es10/client.c
@@ -0,0 +1,13 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define SERVER_PORT 1314
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
