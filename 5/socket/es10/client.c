0eafb5f8cc5628bab979bb4a402d984eb9970a9e
niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2024-11-05 07:44:20 -0800

    esercizio 10

--- 5/socket/es10/client.c
+++ 5/socket/es10/client.c
@@ -6,8 +6,66 @@
 #include <unistd.h>
 
 #define SERVER_PORT 1314
+#define DIM 50
 
 int main(int argc, char **argv)
 {
+    struct sockaddr_in servizio;
+    int socketfd;
+    char str1[DIM];
+    char str2[DIM];
+    char risposta[DIM];
+
+    servizio.sin_family = AF_INET;
+    servizio.sin_port = htons(SERVER_PORT);
+    servizio.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    //creo il socket
+    socketfd = socket(AF_INET,SOCK_STREAM,0);
+
+    //verifco se la creazione del socket sia riuscita
+    if(socketfd < 0)
+    {
+        perror("Errore nella creazione del socket \n");
+        exit(-1);
+    }
+
+    //faccio inserire le stringhe all'utente
+    printf("Inserire stringa 1 \n");
+    scanf("%s",str1);
+    printf("Inserire stringa 2 \n");
+    scanf("%s",str2);
+
+    //creo la connessione con il server e verifico se è riuscita
+    if(connect(socketfd, (struct sockaddr *)&servizio, sizeof(servizio)) < 0)
+    {
+        perror("Errore nella connessione con il server \n");
+        exit(-1);
+    }
+
+    //mando al server le due stringhe e ne verifico la riuscita
+    if(write(socketfd, str1, DIM) < 0)
+    {
+        perror("Errore nella scrittura della stringa 1 al server \n");
+    }
+    if(write(socketfd, str2, DIM) < 0)
+    {
+        perror("Errore nella scrittura della stringa 1 al server \n");
+    }
+
+    //leggo dal server la risposta e ne verifico la riuscita
+    if(read(socketfd, risposta, DIM) < 0)
+    {
+        perror("Errore nella lettura della risposta dal server \n");
+        exit(-1);
+    }
+
+    //stampo la risposta ricevuta dal server
+    printf("La stringa più lunga è %s \n", risposta);
+
+
+    //chiudo il socket
+    close(socketfd);
+
     return 0;
 }
--- 5/socket/es10/server.c
+++ 5/socket/es10/server.c
@@ -3,11 +3,102 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
+#include <string.h>
 
 #define SERVER_PORT 1314
+#define DIM 50
+
+char * longestString(char *str1, char* str2)
+{
+    char *risposta = malloc(sizeof(char)*DIM);
+
+    if (strlen(str1) > strlen(str2))
+    {
+        risposta = str1;
+    }
+    else if (strlen(str1) < strlen(str2)) {
+
+    risposta = str2;
+    }
+    else{
+        risposta = "le stringhe sono lunghe uguali";
+    }
+
+    return risposta;
+}
 
 int main(int argc, char **argv)
 {
+    struct sockaddr_in servizio,addCLient;
+    int socketfd,soa,size = sizeof(addCLient);
+    char str1[DIM];
+    char str2[DIM];
+    char *risposta = malloc(sizeof(char)*DIM);
+
+    servizio.sin_family = AF_INET;
+    servizio.sin_addr.s_addr = htonl(INADDR_ANY);
+    servizio.sin_port = htons(SERVER_PORT);
+
+    //creo il socket
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(socketfd < 0)
+    {
+        perror("Errore nella creazione del socket \n");
+        exit(-1);
+    }
+
+    //faccio la bind e ne verifico la riuscita
+    if(bind(socketfd, (struct sockaddr*)&servizio, sizeof(servizio)))
+    {
+        perror("Errore nella bind \n");
+        exit(-1);
+    }
+
+    //metto il server in ascolto con un massimo di 10 client
+    listen(socketfd, 10);
+
+    while (1) {
+
+        printf("Server in ascolto... \n");
+
+        //accetto la connessione
+        soa = accept(socketfd, (struct sockaddr*)&addCLient, &size);
+
+        //verifco la riuscita della connessione
+        if(soa < 0)
+        {
+            perror("Errore nella connessione con il client \n");
+            exit(-1);
+        }  
+
+        //leggo le stringhe inviate dal client e ne verifico la riuscita
+        if(read(soa, str1, DIM) < 0)
+        {
+            perror("Errore nella lettura della stringa 1 dal client \n");
+        }
+        if(read(soa, str2, DIM) < 0)
+        {
+            perror("Errore nella lettura della stringa 2 dal client \n");
+        }
+
+        //verifico quale stringa è la più lunga
+        risposta = longestString(str1, str2);
+
+        //scrivo al server la risposta
+        if(write(soa, risposta, DIM) < 0)
+        {
+            perror("Errore nell'invio della risposta al client \n");
+            exit(-1);
+        }
+
+        printf("Stringa risposta inviata al client \n");
+
+        //chiudo la connessione col client
+        close(soa);
+    }
+
     return 0;
 }
